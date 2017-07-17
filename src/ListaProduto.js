66cd505a46e8206e4cd08ee9f9b88327845fd417
Raquel <raquel.zatta@gmail.com>
2017-07-16 23:50:54 -0300

    ml-app

--- src/ListaProduto.js
+++ src/ListaProduto.js
@@ -20,7 +20,7 @@ export class ListaProduto extends Component{
 		});
 	}
 	
-	
+	                    
 			
 	render(){
 		return (
--- src/ProdutoItem.js
+++ src/ProdutoItem.js
@@ -8,9 +8,11 @@ export class ProdutoItem extends Component{
 		return (
                     <a style={{color:"#000"}} href={"/itens/" + this.props.produto.item.price.currency + "/"}>
 					<div className="containerProduto">
-                        <div className="imgProduto"><img src="#" width="150" height="150"/></div>
-                        <div className="valor"><p className="pValor">{this.props.produto.item.price.currency}{new Intl.NumberFormat('pt-BR').format(this.props.produto.item.price.amount)}<sup>{this.props.produto.item.price.decimals}</sup></p></div>
-                        <div className="imgProduto"><img src={require('./img/circle.png')} width="20" height="20"/></div>
+                        
+						<div className="imgProduto"><img src={"http://lorempixel.com/150/150/abstract/"} width="150" height="150"/></div>
+
+						<div className="valor"><p className="pValor">{this.props.produto.item.price.currency}{new Intl.NumberFormat('pt-BR').format(this.props.produto.item.price.amount)}<sup>{this.props.produto.item.price.decimals}</sup></p></div>
+                        <div className="imgProduto"><img src={require('./img/circle.png')} width="15" height="15"/></div>
                         <div className="descricaoEsq"><p className="pDescricaoEsq">{this.props.produto.item.title}</p></div>
                         <div className="descricaoDir"><p className="pDescricaoDir">{this.props.produto.author.name}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p></div>                      
                     </div>
